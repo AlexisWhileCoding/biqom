60f50328c1604efb32b3f17347f0ea6d2e722007
Alexis Duchatelet <alexis.duchatelet@gmail.com>
2020-08-24 15:00:47 +0200

    routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get "dashboard", to: "pages#dashboard"
+  get "my_activities", to: "plannings#my_activities", as: :my_activities
 
-  resources :activities
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :user_challenges, only: [:index, :edit, :create, :update]
+
+  resources :activities, only: [:index, :show] do
+    post "plannings", to: "plannings#create"
+  end
+  
 end
