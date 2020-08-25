0c8b3dbd1aff837e33310dd06db32ab9c0219d11
Alexis Duchatelet <alexis.duchatelet@gmail.com>
2020-08-25 14:58:13 +0200

    added structure for index + cloudinary & active storage setup

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,4 @@
 class Activity < ApplicationRecord
   has_many :challenges
+  has_one_attached :photo
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "open-uri"
+
+file = URI.open('https://images.unsplash.com/photo-1593810450979-79e8584f76b9?ixlib=rb-1.2.1&auto=format&fit=crop&w=1652&q=80')
+activity = Activity.new(name: 'Yoga', description: "A 30-day beginners Yoga activity. Great way to get started and enjoy this popular activity", type: "Sports", days: 30)
+activity.photo.attach(io: file, filename: 'yoga.png', content_type: 'image/png')
