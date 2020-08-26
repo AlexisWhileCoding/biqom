78de4faff3cef7c34c61a8f9c42001f96b88554a
Alexis Duchatelet <alexis.duchatelet@gmail.com>
2020-08-26 17:19:00 +0200

    Added challenge for testing - added index method for user_challenges

--- app/controllers/user_challenges_controller.rb
+++ app/controllers/user_challenges_controller.rb
@@ -1,6 +1,14 @@
 class UserChallengesController < ApplicationController
   def index
-    @user_challenges = UserChallenge.all
+    @user_challenges = []
+    current_user.plannings.each do |planning|
+      next unless planning.start_date >= Date.today && (planning.start_date + planning.activity.days) < Date.today 
+      position = (Date.today - planning.start_date).days
+      challenge =  planning.activity.challenges.find_by(position: position)
+      @user_challenges << current_user.user_challenges.find_by(challenge: challenge)
+    end
+
+    @user_challenges
   end
 
   def edit
--- app/models/planning.rb
+++ app/models/planning.rb
@@ -1,4 +1,14 @@
 class Planning < ApplicationRecord
   belongs_to :user
   belongs_to :activity
+
+  has_many :user_challenges
+
+  after_create :create_user_challenges
+
+  def create_user_challenges
+    activity.challenges.each do |challenge|
+      user_challenges.create(challenge: challenge, start_time: start_time)
+    end
+  end
 end
