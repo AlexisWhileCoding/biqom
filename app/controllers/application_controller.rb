a9c8abe484852ce2cb795846eefa0409f90e123b
Alexis Duchatelet <alexis.duchatelet@gmail.com>
2020-09-04 10:18:18 +0200

    meta tags added and favicon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,5 +10,9 @@ class ApplicationController < ActionController::Base
     stored_location_for(resource) || activities_path
   end
 
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
 end
 
